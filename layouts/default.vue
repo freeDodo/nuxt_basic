921a646ee9fee442d9ac3fd481cbd5b904ccf768
FreeDoDo <madcurieux@gmail.com>
2022-11-16 10:21:49 +0900

    default Layout 생성

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,25 @@
 <template>
-  <div>
-    <header style="height: 80px; background: black"></header>
-    <Nuxt />
+  <div id="app">
+    <div id="container">
+      <header class="header">
+        <div class="header__logo">
+          <h1>
+            <NuxtLink to="/"> </NuxtLink>
+          </h1>
+        </div>
+        <nav class="header__layout--nav primary">
+          <NuxtLink to="/">Home</NuxtLink>
+          <NuxtLink to="/nuxt/basic">page1</NuxtLink>
+          <NuxtLink to="/nuxt/1">page2</NuxtLink>
+        </nav>
+        <nav class="header__layout--nav">
+          <NuxtLink to="/nuxt/2">other</NuxtLink>
+        </nav>
+      </header>
+      <router-view class="main" />
+    </div>
   </div>
 </template>
 
-<script>
-export default {};
-</script>
-
-<style>
+<style src="@/assets/css/layout.css">
 </style>
---
+++ pages/nuxt/basic.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>BASIC</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
